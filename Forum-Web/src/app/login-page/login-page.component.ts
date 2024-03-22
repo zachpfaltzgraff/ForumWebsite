4b5b1bff04f01edc6d379e4f0758327037555d3d
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-03-21 20:05:05 -0400

    login page done

--- Forum-Web/src/app/login-page/login-page.component.ts
+++ Forum-Web/src/app/login-page/login-page.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, FloatLabelModule, InputTextModule, PasswordModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-
+  value: string | undefined;
 }

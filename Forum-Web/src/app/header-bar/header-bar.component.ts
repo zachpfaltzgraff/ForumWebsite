b3cba5276c8ebff155477a2489dc31f4988e3819
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2024-03-22 18:37:10 -0400

    header bar done

--- Forum-Web/src/app/header-bar/header-bar.component.ts
+++ Forum-Web/src/app/header-bar/header-bar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header-bar',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.css'
 })
 export class HeaderBarComponent {
-
+  
 }

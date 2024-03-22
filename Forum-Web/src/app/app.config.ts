b8e3bbcc543f061b1c3e49184e227ceae41419d5
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-03-21 20:43:49 -0400

    typescript of login page is done

--- Forum-Web/src/app/app.config.ts
+++ Forum-Web/src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),
+  MessageService, BrowserAnimationsModule, CommonModule, BrowserModule, provideAnimations()]
 };
--- Forum-Web/src/app/login-page/login-page.component.ts
+++ Forum-Web/src/app/login-page/login-page.component.ts
@@ -3,14 +3,70 @@ import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [ButtonModule, FloatLabelModule, InputTextModule, PasswordModule],
+  imports: [ReactiveFormsModule, 
+    ButtonModule, 
+    FloatLabelModule, 
+    InputTextModule, 
+    PasswordModule,
+    ToastModule,
+    CommonModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-  value: string | undefined;
+  constructor(private messageService: MessageService) {}
+
+  onSignUp() {
+    if (this.signupForm.valid) {
+      console.log("valid form")
+      const formData = {
+        username: this.signupForm.value.username,
+        email: this.signupForm.value.email,
+        password: this.signupForm.value.password,
+      }
+      console.log(formData);
+
+      // send to cognito
+
+    } else {
+      this.messageService.add({ key: 'bc', severity: 'error', summary: 'Error', detail: 'Invalid Form' });
+    }
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log("valid form")
+      const formData = {
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password,
+      }
+      console.log(formData);
+
+      // send to cognito
+      
+    } else {
+      this.messageService.add({ key: 'bc', severity: 'error', summary: 'Error', detail: 'Invalid Form' });
+    }
+  }
+
+  signupForm = new FormGroup( {
+    email: new FormControl('', [Validators.email, Validators.required]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', Validators.required)
+  })
+
+  loginForm = new FormGroup( {
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', Validators.required)
+  })
 }

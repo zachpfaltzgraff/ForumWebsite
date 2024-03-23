ce8a03b13ce00aa041679e833ac1d19d16063a1d
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2024-03-23 17:15:31 -0400

    created home page

--- Forum-Web/src/app/app.routes.ts
+++ Forum-Web/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
-    { path:'login', component: LoginPageComponent}
+    { path:'login', component: LoginPageComponent},
+    { path: '', component: HomePageComponent}
 ];

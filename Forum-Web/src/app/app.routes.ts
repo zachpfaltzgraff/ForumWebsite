73c3913454162f81e5f82fe47b742d2ad36ccf7c
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2024-04-09 13:56:44 -0400

    feedback page created

--- Forum-Web/src/app/app.routes.ts
+++ Forum-Web/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FeedbackPageComponent } from './feedback-page/feedback-page.component';
 
 export const routes: Routes = [
     { path:'login', component: LoginPageComponent}, 
-    { path: '', component: HomePageComponent}
+    { path: '', component: HomePageComponent},
+    { path: 'feedback', component: FeedbackPageComponent},
 ];
---
+++ Forum-Web/src/app/feedback-page/feedback-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './feedback-page.component.html',
+  styleUrl: './feedback-page.component.css'
+})
+export class FeedbackPageComponent {
+
+}

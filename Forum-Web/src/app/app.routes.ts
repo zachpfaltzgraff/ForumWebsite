b8256f72839fbefc1c6329f2f2eb514b776a4fe7
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-04-10 21:35:13 -0400

    about us page made

--- Forum-Web/src/app/app.routes.ts
+++ Forum-Web/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { FeedbackPageComponent } from './feedback-page/feedback-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 export const routes: Routes = [
     { path:'login', component: LoginPageComponent}, 
     { path: '', component: HomePageComponent},
     { path: 'feedback', component: FeedbackPageComponent},
+    {path: 'about', component: AboutPageComponent},
 ];

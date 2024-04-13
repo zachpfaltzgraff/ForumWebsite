ab4dacffd17048054172b67a251ccb2a4d15b4fb
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-04-12 21:35:58 -0400

    fixed issue
    this one makes me so happy, such a simple solution to an ongoing issue since i started this project

--- Forum-Web/src/app/app.component.ts
+++ Forum-Web/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { Amplify } from 'aws-amplify';
+import cdkOutput from '../../../../ForumWebCDK/output.json';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,4 +16,22 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Forum-Web';
+
+  constructor() {
+    Amplify.configure({
+      Auth: {
+        Cognito: {
+          userPoolId: cdkOutput.CognitoStack.PoolId,
+          userPoolClientId: cdkOutput.CognitoStack.ClientId,
+          signUpVerificationMethod: 'code',
+          loginWith: {
+            username: true,
+            email: true,
+          }
+        }
+      }
+    });
+  }
+
+  currentConfig = Amplify.getConfig();
 }
--- Forum-Web/src/main.ts
+++ Forum-Web/src/main.ts
@@ -1,24 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import cdkOutput from '../../../ForumWebCDK/output.json';
-import { Amplify } from 'aws-amplify';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error("ERROR:" , err));
-
-    Amplify.configure({
-      Auth: {
-        Cognito: {
-          userPoolId: cdkOutput.CognitoStack.PoolId,
-          userPoolClientId: cdkOutput.CognitoStack.ClientId,
-          signUpVerificationMethod: 'code',
-          loginWith: {
-            username: true,
-            email: true,
-          }
-        }
-      }
-    }); 
-
-    const currentConfig = Amplify.getConfig();
